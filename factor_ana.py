6267be9a836b68a91eef83a6c3a93288ef19de55
dknt <963725175@qq.com>
2023-09-24 21:55:13 +0300

    code

--- factor_ana.py
+++ factor_ana.py
@@ -16,7 +16,7 @@ matplotlib.rcParams[u'font.sans-serif'] = ['simhei']
 matplotlib.rcParams['axes.unicode_minus'] = False
 
 # 读取数据
-data = pd.read_csv('data/carbon.csv')
+data = pd.read_csv('data/factor.csv')
 
 # 选择特征
 # cols = ['population',  # 人口
@@ -29,11 +29,19 @@ data = pd.read_csv('data/carbon.csv')
 #         '人口增长率', 'GDP增长率', '人均GDP增长率', '能源强度增长率', '能源碳排强度增长率'
 #         ]
 
-cols = ['GDP总量', '人均GDP', '能源消费总量']
+cols = [
+        'GDP增长率',
+        '人口增长率',
+        '人均能源消耗',
+        '人均GDP增长率',
+        '能源消耗增长率',
+        '人均能源消耗增长率',
+        'fossilProp_2total'
+]
 
 # 训练模型
 model = xgb.XGBRegressor(max_depth=8, learning_rate=0.05, n_estimators=150)
-model.fit(data[cols], data['碳排放总量', 'gdp_eng'].values)
+model.fit(data[cols], data['carbEmisInc_total'].values)
 
 # # 使用 feature importance 解释 xgboost
 plt.figure(figsize=(10, 10))
@@ -42,7 +50,6 @@ plt.xticks(range(len(cols)), cols, rotation=-45, fontsize=14)
 plt.title('特征重要程度', fontsize=14)
 plt.show()
 
-##############################################33
 # 使用 shap 解释 xgboost
 explainer = shap.TreeExplainer(model)
 shap_values = explainer.shap_values(data[cols])
@@ -54,15 +61,14 @@ player_explainer = pd.DataFrame()
 player_explainer['feature'] = cols
 player_explainer['feature_value'] = data[cols].iloc[j].values
 player_explainer['shap_value'] = shap_values[j]
-
-# 单个绘图
 shap.initjs()
 shap.force_plot(explainer.expected_value, shap_values[j], data[cols].iloc[j])
 
 
 ## 总体分析
+# 总体特征可视化
 shap.summary_plot(shap_values, data[cols])
-# 绝对值影响程度
+#
 shap.summary_plot(shap_values, data[cols], plot_type="bar")
 
 
@@ -70,11 +76,9 @@ shap.summary_plot(shap_values, data[cols], plot_type="bar")
 for temp in cols:
     shap.dependence_plot(temp, shap_values, data[cols], interaction_index=None, show=False)
 
-
-
-## 多个变量交互分析
+# 多个变量交互分析
 shap_interaction_values = shap.TreeExplainer(model).shap_interaction_values(data[cols])
 shap.summary_plot(shap_interaction_values, data[cols], max_display=4)
 
-## 两个变量交互影响
-shap.dependence_plot('GDP总量', shap_values, data[cols], interaction_index='人均能源消耗', show=False)
+
+shap.dependence_plot('population', shap_values, data[cols], interaction_index='engCons_total', show=False)
